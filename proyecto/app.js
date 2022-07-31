762fdc7c6296d38146d070bfde2f21894f66b02e
Azexar <kevschi@hotmail.com>
2022-07-31 16:43:44 -0300

    agregados a app.js

--- proyecto/app.js
+++ proyecto/app.js
@@ -13,6 +13,22 @@ app.get("/productos",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/productDetail.html"))
 })
 
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+
+app.get("/productCart",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/productCart.html"))
+})
+
+app.get("/productDetail",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/productDetail.html"))
+})
+
 app.listen(3000,()=>{
     console.log("funcionando");
 })
