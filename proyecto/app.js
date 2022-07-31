c5d0de803a438be71e2f30e6fb62c305debc33c6
Azexar <kevschi@hotmail.com>
2022-07-31 16:51:22 -0300

    modificacion app.js

--- proyecto/app.js
+++ proyecto/app.js
@@ -9,10 +9,6 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))
 })
 
-app.get("/productos",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/productDetail.html"))
-})
-
 app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/login.html"))
 })
