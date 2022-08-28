a892973dc9c090182bea8f676a5e5321fda8465c
JohanLeon07 <johanleon0705@gmail.com>
2022-08-28 00:20:16 -0500

    index dinamico

--- proyecto/controller/mainController.js
+++ proyecto/controller/mainController.js
@@ -1,10 +1,21 @@
+const fs = require("fs")
 const path = require("path");
+const productsFilePath = path.join(__dirname, '../data/productos.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const mainController = {
     index:(req,res)=>{
-        res.render("index")
+        res.render("index",{productos: products })
     },
-    
+    search: (req, res) => {
+
+		let search = req.query.keywords;
+		search = search.toLowerCase();
+		
+		const resultado = products.filter((p) => p.name.toLowerCase().includes(search));
+
+		res.render("results", { productos: resultado, search: search })
+	},
 }
 
 module.exports = mainController
--- proyecto/routers/indexRouter.js
+++ proyecto/routers/indexRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mainController=require("../controller/mainController")
 
 router.get("/",mainController.index);
-
+router.get("/search",mainController.search);
 
 
 module.exports = router;
