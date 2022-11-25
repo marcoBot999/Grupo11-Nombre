1da8ee3f1779d0569e09dea8e54a9c52f779fc7d
Azexar <kevschi@hotmail.com>
2022-11-24 23:14:52 -0300

    api

--- proyecto/controller/api/userApiController.js
+++ proyecto/controller/api/userApiController.js
@@ -10,21 +10,22 @@ const userApiController= {
     list: (req,res)=>{
     db.User.findAll(
         {
-                attributes: ['firstname']
+                attributes: ['firstname',"lastname","email","id_user"]
         }
     )
     .then(lista=> {
         return res.json (
             {total:lista.length,
             data:lista,
-           id: lista.id_user,  
-        
     })
         
     })
     },detail: (req, res) => {
 
-        db.User.findByPk(req.params.id)
+        db.User.findByPk(req.params.id,
+            {
+                attributes: ['firstname',"lastname","email","id_user"]
+        })
         .then((lista) => {
             res.json({ 
                 data : lista,
